42102d7cfeb0bf6fafec68ac1d3cd8ea13e3bf38
Zachary Skiba <zskiba@hawk.iit.edu>
2025-09-27 12:04:07 -0500

    dev changes

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+# Import the main db instance and base models
+from .models import *
+
+
+# Make sure all models are available when importing from models
+__all__ = [
+    'db', 'User', 'Group', 'Category', 'Expense', 'ExpenseParticipant', 
+    'Balance', 'Settlement', 'RecurringPayment', 'user_groups',
+]
